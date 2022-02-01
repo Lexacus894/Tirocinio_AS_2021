f350cae152956c12c59a802541250ca48d704774
AleSer <57143788+Lexacus894@users.noreply.github.com>
2022-02-01 11:41:30 +0100

    Create FeatureCommand.java
    Creazione classe FeatureCommand (prototipo)

---
+++ GeneratoreFeature/JavaT_Tirocinio/src/dataset/FeatureCommand.java
@@ -0,0 +1,109 @@
+package dataset;
+
+public class FeatureCommand {
+	
+	private String SoftwareName;
+	private String FQNClass;
+	private int ClassDeclarationKeyword;
+	private int MethodDeclarationKeyword;
+	private int ClassType;
+	private int ExecutesCommand;
+	private int AddCommandMethod;
+	private int HasSuperclass;
+	private int ImplementsInterfaces;
+
+	public FeatureCommand(String softwareName, String fqnClass, int classDeclaration, int methodDeclaration, int classType, int executesCommand, 
+			int addCommand, int hasSuperclass, int implementsInterfaces ) {
+		this.SoftwareName = softwareName;
+		this.FQNClass = fqnClass;
+		this.ClassDeclarationKeyword = classDeclaration;
+		this.MethodDeclarationKeyword = methodDeclaration;
+		this.ClassType = classType;
+		this.ExecutesCommand = executesCommand;
+		this.AddCommandMethod = addCommand;
+		this.HasSuperclass = hasSuperclass;
+		this.ImplementsInterfaces = implementsInterfaces;
+	}
+
+	public String getSoftwareName() {
+		return SoftwareName;
+	}
+
+	public void setSoftwareName(String softwareName) {
+		SoftwareName = softwareName;
+	}
+
+	public String getFQNClass() {
+		return FQNClass;
+	}
+
+	public void setFQNClass(String fQNClass) {
+		FQNClass = fQNClass;
+	}
+
+	public int getClassDeclarationKeyword() {
+		return ClassDeclarationKeyword;
+	}
+
+	public void setClassDeclarationKeyword(int classDeclarationKeyword) {
+		ClassDeclarationKeyword = classDeclarationKeyword;
+	}
+
+	public int getMethodDeclarationKeyword() {
+		return MethodDeclarationKeyword;
+	}
+
+	public void setMethodDeclarationKeyword(int methodDeclarationKeyword) {
+		MethodDeclarationKeyword = methodDeclarationKeyword;
+	}
+
+	public int getClassType() {
+		return ClassType;
+	}
+
+	public void setClassType(int classType) {
+		ClassType = classType;
+	}
+
+	public int getExecutesCommand() {
+		return ExecutesCommand;
+	}
+
+	public void setExecutesCommand(int executesCommand) {
+		ExecutesCommand = executesCommand;
+	}
+
+	public int getAddCommandMethod() {
+		return AddCommandMethod;
+	}
+
+	public void setAddCommandMethod(int addCommandMethod) {
+		AddCommandMethod = addCommandMethod;
+	}
+
+	public int getHasSuperclass() {
+		return HasSuperclass;
+	}
+
+	public void setHasSuperclass(int hasSuperclass) {
+		HasSuperclass = hasSuperclass;
+	}
+
+	public int getImplementsInterfaces() {
+		return ImplementsInterfaces;
+	}
+
+	public void setImplementsInterfaces(int implementsInterfaces) {
+		ImplementsInterfaces = implementsInterfaces;
+	}
+	
+	@Override
+	  public String toString() {
+		  
+		  return " VettoreFeatureCommand [SoftwareName = " + this.SoftwareName + ", FQNClass = " + this.FQNClass
+		  		+ ", ClassDeclarationKeyword = " + this.ClassDeclarationKeyword + ", MethodDeclarationKeyword = " + this.MethodDeclarationKeyword
+		  		+ ", ClassType = " + this.ClassType + ", ExecutesCommand = " + this.ExecutesCommand + ", AddCommand = " + this.AddCommandMethod
+		  		+ ", HasSuperclass = "+ this.HasSuperclass + ", ImplementsInterfaces = " + this.ImplementsInterfaces +"]";
+	  
+	  }
+}
